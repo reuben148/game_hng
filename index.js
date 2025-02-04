1621c8bee24530056ab800a23a1aa647a3e0f887
reuben148 <reubenluka555@gmail.com>
2025-02-05 00:03:05 +0100

    last push

---
+++ index.js
@@ -0,0 +1,55 @@
+const border = document.getElementById("border");
+const buttons = document.querySelectorAll(".btn button");
+const scoreDisplay = document.getElementById("score");
+const resetButton = document.getElementById("reset");
+const message = document.getElementById("message");
+
+let colors = ["red", "blue", "green", "yellow", "purple", "gray"];
+let targetColor = "";
+let score = 0;
+
+
+function startGame() {
+    targetColor = colors[Math.floor(Math.random() * colors.length)];
+    border.style.backgroundColor = targetColor;
+    scoreDisplay.textContent = `Score: ${score}`;
+    message.textContent = ""; 
+}
+
+
+buttons.forEach(button => {
+    button.style.backgroundColor = button.id;
+    button.addEventListener("click", () => {
+        if (button.id === targetColor) {
+            score++;
+            scoreDisplay.textContent = `Score: ${score}`;
+            message.textContent = "🎉 Congratulations!";
+            message.style.color = "green";
+
+            
+            setTimeout(() => {
+                message.textContent = "";
+                startGame();
+            }, 1000);
+        } else {
+            score = 0;
+            scoreDisplay.textContent = `Score: ${score}`;
+            message.textContent = "😢 opps Try Again!";
+            message.style.color = "red";
+
+            
+            setTimeout(() => {
+                message.textContent = "";
+            }, 1000);
+        }
+    });
+});
+
+
+resetButton.addEventListener("click", () => {
+    score = 0;
+    startGame();
+});
+
+
+startGame();
